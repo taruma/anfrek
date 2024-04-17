fe340eac361885e1409c9a5f91a556b75e0f2fd1
taruma sakti <hi@taruma.info>
2024-04-17 19:33:20 +0700

    Update pylayoutfunc.py with new functions and documentation

--- pylayoutfunc.py
+++ pylayoutfunc.py
@@ -1,10 +1,22 @@
-from __future__ import annotations
-import plotly.graph_objects as go
+"""Functions to create layout components for Dash apps."""
+
+from collections.abc import Iterable
 from dash import dcc, dash_table
+import plotly.graph_objects as go
 from pytemplate import fktemplate
 
 
 def graph_as_staticplot(figure: go.Figure, config: dict = None) -> dcc.Graph:
+    """
+    Converts a plotly figure into a static plot using dcc.Graph.
+
+    Parameters:
+        figure (go.Figure): The plotly figure to be converted.
+        config (dict, optional): Additional configuration options for the dcc.Graph component.
+
+    Returns:
+        dcc.Graph: The converted static plot as a dcc.Graph component.
+    """
     config = {"staticPlot": True} if config is None else config
     return dcc.Graph(figure=figure, config=config)
 
@@ -18,7 +30,27 @@ def create_table_layout(
     deletable=True,
     renamable=False,
 ):
-    from collections.abc import Iterable
+    """
+    Create a table layout using Dash DataTable.
+
+    Args:
+        dataframe (pandas.DataFrame): The input dataframe to be displayed in the table.
+        idtable (str): The ID of the DataTable component.
+        filename (str, optional): The filename associated with the table.
+            Defaults to None.
+        filedate (str, optional): The file date associated with the table.
+            Defaults to None.
+        editable (list or bool, optional): Specifies the editability of columns.
+            If a list is provided, it should have the same length
+            as the number of columns in the dataframe. Defaults to False.
+        deletable (bool, optional): Specifies whether rows can be deleted. Defaults to True.
+        renamable (bool, optional): Specifies whether columns can be renamed. Defaults to False.
+
+    Returns:
+        dash_table.DataTable: The created DataTable component.
+
+    """
+    _, _ = filename, filedate
 
     new_dataframe = dataframe.rename_axis("DATE").reset_index()
     new_dataframe.DATE = new_dataframe.DATE.dt.date
